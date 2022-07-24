0647f2d240ce8deb1143ca62f18d937ef6a4922b
acs <acs@DESKTOP-G4M6QQA>
2022-07-24 15:46:11 +0530

    commit in employee management project

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO employee VALUES (101, 'akashmalviya@gmail.com', 'Akash', 'Malviya');
+INSERT INTO employee VALUES (102, 'rohitsharma@gmail.com', 'Rohit', 'Sharma');
+INSERT INTO employee VALUES (103, 'manishpandya@gmail.com', 'Manish', 'Pandya');
+INSERT INTO employee VALUES (104, 'rahulvarma@gmail.com', 'Rahul', 'Varma');
+INSERT INTO employee VALUES (105, 'vimalsundar@gmail.com', 'Vimal', 'Sundar');
+
+INSERT INTO USERS VALUES(101, 'admin', 'admin');
+INSERT INTO ROLES VALUES(101, 'ADMIN');
+INSERT INTO USERS_ROLES VALUES (101, 101);
